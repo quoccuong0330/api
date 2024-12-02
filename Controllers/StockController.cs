fb6fd381c9fdf8ed37eb5c445df7de257a9e71d0
Trần Quốc Cường <quoccuong0330@gmail.com>
2024-12-02 20:58:24 +0700

    post_put_delete_method

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,5 +1,8 @@
+using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPI.Data;
+using WebAPI.DTOs.Stock;
 using WebAPI.Mappers;
 using WebAPI.Models;
 
@@ -22,7 +25,44 @@ public class StockController : ControllerBase {
     [HttpGet("{id}")]
     public IActionResult GetStockById([FromRoute] int id) {
         var stockById = _context.Stock.Find(id);
-        
         return stockById != null ? Ok(stockById.ToStockDto()) : NotFound();
     }
+
+    [HttpPost]
+    public IActionResult CreateNewStock([FromBody] CreateStockRequestDTO createStock) {
+        var stockModel = createStock.ToStockFromCreateDTO();
+        _context.Add(stockModel);
+        
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetStockById), new {id = stockModel.Id}, stockModel.ToStockDto());
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateStockById([FromRoute] int id, [FromBody] UpdateStockRequestDTO updateStock) {
+        var stockModel = _context.Stock.FirstOrDefault(x => x.Id == id);
+        if (stockModel == null) {
+            return NotFound();
+        }
+
+        stockModel.Industry = updateStock.Industry;
+        stockModel.MarketCap = updateStock.MarketCap;
+        stockModel.Symbol = updateStock.Symbol;
+        stockModel.CompanyName = updateStock.CompanyName;
+        stockModel.Purchase = updateStock.Purchase;
+        stockModel.LastDiv = updateStock.LastDiv;
+
+        _context.SaveChanges();
+        return Ok(stockModel);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteStockById([FromRoute] int id) {
+        var stockModel = _context.Stock.FirstOrDefault(x=> x.Id == id);
+        if (stockModel == null) return NotFound();
+        
+        _context.Stock.Remove(stockModel);
+        _context.SaveChanges();
+        return NoContent();
+    }
+    
 }
